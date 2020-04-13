2677f04ef8c47b99a23a6d24777145f4cbdc3d7a
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-13 11:22:59 +1000

    Fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
+const currentYear = new Date().getFullYear();
 export default function Footer() {
   return (
-    <a href="https://icons8.com/icon/122505/oak-tree">
-      Oak Tree icon by Icons8
-    </a>
+    <div className="py-4 px-12 sm:px-4 border-t-1 text-center">
+      &copy; Copyright {currentYear}
+      <a className="ml-4" href="https://icons8.com/icon/122505/oak-tree">
+        Oak Tree icon by Icons8
+      </a>
+  </div>
   );
 }
--- components/PostPreview.js
+++ components/PostPreview.js
@@ -1,6 +1,6 @@
 export default function PostPreview() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+    <div className="max-w-sm overflow-hidden shadow-lg m-4">
       <img
         className="w-full"
         src="https://picsum.photos/600/400/?random"
@@ -9,20 +9,20 @@ export default function PostPreview() {
 
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p className="text-gray-700 text-base">
+        <p className="text-gray-700 text-body">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
           nihil.
         </p>
       </div>
       <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
           #photography
         </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
           #travel
         </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700">
           #winter
         </span>
       </div>

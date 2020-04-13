4cc8680e22842961e6de80f1f932c53fd4013d00
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-13 17:04:02 +1000

    change post into a link

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -1,31 +1,33 @@
 export default function PostPreview() {
   return (
-    <div className="max-w-sm overflow-hidden shadow-lg m-4">
-      <img
-        className="w-full"
-        src="https://picsum.photos/600/400/?random"
-        alt="Sunset in the mountains"
-      />
+    <a href="">
+      <div className="max-w-sm overflow-hidden shadow-lg m-4">
+        <img
+          className="w-full"
+          src="https://picsum.photos/600/400/?random"
+          alt="Sunset in the mountains"
+        />
 
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p className="text-gray-700 text-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p className="text-gray-700 text-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+            nihil.
+          </p>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #photography
+          </span>
+          <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #travel
+          </span>
+          <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700">
+            #winter
+          </span>
+        </div>
       </div>
-      <div className="px-6 py-4">
-        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #photography
-        </span>
-        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #travel
-        </span>
-        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700">
-          #winter
-        </span>
-      </div>
-    </div>
+    </a>
   );
 }

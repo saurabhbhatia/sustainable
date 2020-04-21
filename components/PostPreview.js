056561ac607c94f3e7b8c2abac2fe60055b23b6d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-21 21:59:17 +1000

    improve post-preview

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -4,30 +4,15 @@ class PostPreview extends Component {
   render() {
     const post = this.props.post;
     return (
-      <a href={`/blog/${post.uid}`}>
-        <div className="max-w-sm overflow-hidden shadow-2xl m-4">
+      <a href={`/blog/${post.uid}`} className="max-w-sm overflow-hidden shadow-lg hover:shadow-2xl m-4 border">
           <img
             className="w-full"
             src="https://picsum.photos/600/400/?random"
             alt="Sunset in the mountains"
           />
-
-          <div className="px-6 py-4">
+          <div className="px-6 py-4 text-center">
             <div className="font-bold text-xl mb-2">{post.data.title[0].text}</div>
-            <p className="text-gray-700 text-body"></p>
           </div>
-          <div className="px-6 py-4">
-            <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #photography
-            </span>
-            <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #travel
-            </span>
-            <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700">
-              #winter
-            </span>
-          </div>
-        </div>
       </a>
     );
   }

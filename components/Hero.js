1cc3a61d5fb6191b6ddfef9e005eb77211435a9e
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-25 01:22:29 +1000

    improve homepge cards

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="flex flex-column w-full border-b-1">
       <img className="w-1/2 sm:w-full" src={imgUrl}></img>
       <div className="w-1/2 sm:hidden flex items-center text-center">
-        <h1 className="text-5xl sm:text-2xl">A blog about living life sustainably as an individual.</h1>
+        <h1 className="lg:text-5xl md:text-3xl sm:text-2xl">A blog about living life sustainably as an individual.</h1>
       </div>
     </div>
   );
--- components/PostPreview.js
+++ components/PostPreview.js
@@ -4,16 +4,22 @@ class PostPreview extends Component {
   render() {
     const post = this.props.post;
     return (
-      <a href={`/blog/${post.uid}`} className="max-w-sm overflow-hidden shadow-lg hover:shadow-2xl m-4 border">
-          <img
-            className="w-full"
-            src="https://picsum.photos/600/400/?random"
-            alt="Sunset in the mountains"
-          />
-          <div className="px-6 py-4 text-center">
-            <div className="font-bold text-xl mb-2">{post.data.title[0].text}</div>
-          </div>
-      </a>
+      <a
+        href={`/blog/${post.uid}`}
+        className="my-1 p-1 w-full md:w-1/2 lg:my-2 lg:p-2 lg:w-1/3 hover:border hover:shadow-2xl"
+      >
+        <article className="overflow-hidden rounded border"></article>
+        <img
+          alt="Placeholder"
+          className="block h-auto w-full"
+          src="https://picsum.photos/600/400/?random"
+        />
+        <header className="flex items-center justify-center leading-tight p-2 md:p-4">
+          <h1 className="text-xl no-underline hover:underline text-black">
+            {post.data.title[0].text}
+          </h1>
+        </header>
+       </a>
     );
   }
 }

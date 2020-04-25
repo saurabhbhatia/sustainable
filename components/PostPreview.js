647367a60fb4593a6edf696b43cafe142c3503ee
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-25 21:19:25 +1000

    Update layout

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -8,18 +8,38 @@ class PostPreview extends Component {
         href={`/blog/${post.uid}`}
         className="my-1 p-1 w-full md:w-1/2 lg:my-2 lg:p-2 lg:w-1/3 hover:border hover:shadow-2xl"
       >
-        <article className="overflow-hidden rounded border"></article>
         <img
           alt="Placeholder"
           className="block h-auto w-full"
-          src="https://picsum.photos/600/400/?random"
+          src={post.data.previewimage.url}
         />
-        <header className="flex items-center justify-center leading-tight p-2 md:p-4">
+        <div className="flex items-center leading-tight py-2 md:py-4">
           <h1 className="text-xl no-underline hover:underline text-black">
             {post.data.title[0].text}
           </h1>
-        </header>
-       </a>
+        </div>
+        <div className="flex items-center">
+          <img
+            className="w-10 h-10 rounded-full mr-4"
+            src="https://picsum.photos/600/400/?random"
+            alt="Avatar of Anuja Sethi"
+          />
+          <div className="text-lg">
+            <p className="text-gray-900 leading-none">
+              <span className="text-bold">Author: </span>Anuja Sethi
+            </p>
+            <p className="text-gray-600">
+              <span className="text-bold">Posted: </span>20 Aug 18
+            </p>
+          </div>
+        </div>
+        <br/>
+        <div className="flex items-center">
+            {post.data.subtitle.length > 0 ? (
+              <p className="font-body text-xl">{post.data.subtitle[0].text}</p>
+            ) : null}
+          </div>
+      </a>
     );
   }
 }
--- layouts/index.js
+++ layouts/index.js
@@ -1,8 +1,13 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Head from 'next/head'
 
 const DefaultLayout = ({ children }) => (
     <div>
+      <Head>
+        <title>Sustainable Life</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <header>
         <Navbar />
       </header>

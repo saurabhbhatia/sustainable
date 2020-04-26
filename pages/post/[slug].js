fac74f5775319124745b445274c2692aa3b1fbac
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-26 21:54:45 +1000

    fix blog post view

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { RichText } from "prismic-reactjs";
 import { getBlogPostAPI } from "../../api";
 import DefaultLayout from "../../layouts";
+import { DiscussionEmbed } from "disqus-react";
 
 export default class BlogPost extends Component {
   static async getInitialProps(context) {
@@ -20,19 +21,34 @@ export default class BlogPost extends Component {
     }
     return (
       <DefaultLayout>
-        <div className="flex flex-column w-full border-b-1">
+        <div className="flex flex-column w-full border-b">
           <img className="w-full" src={post.coverimage.url}></img>
         </div>
         <article>
-          <h1 className="text-5xl text-center font-headings border-b">
+          <h1 className="text-5xl text-center font-headings">
             {post.title.length ? post.title[0].text : ""}
           </h1>
-          <div className="flex flex-column flex-start md:mx-2 sm:mx-2 font-body text-xl">
-            <div className="lg:w-1/2 md:w-full sm:w-full inline-block m-0 m-auto text-justify p-8 border-l border-r shadow-xl">
+          <div className="flex flex-column flex-start md:mx-2 sm:mx-2 font-body text-xl border-t">
+            <div className="lg:w-1/2 md:w-full sm:w-full inline-block m-0 m-auto text-justify">
               {RichText.render(post.body)}
             </div>
           </div>
+          {/* <DiscussionEmbed
+            shortname="sustainable-me"
+            config={{
+              url: post.uid,
+              identifier: post.id,
+              title: post.title[0].text,
+            }}
+          /> */}
         </article>
+        <style jsx>{`
+          .block-img {
+            display: flex !important;
+            align-items: center !important;
+            justify-content: center !important;
+          }
+        `}</style>
       </DefaultLayout>
     );
   }

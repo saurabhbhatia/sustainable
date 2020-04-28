01d7ba18fe2ead730ea4a5806d51c0a69a78ac84
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-28 19:34:42 +1000

    update hero image and color

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,10 @@
-const imgUrl = "https://res.cloudinary.com/drg9hguhu/image/upload/v1583056687/electric-car_oquvpr.png";
+const imgUrl = "https://res.cloudinary.com/drg9hguhu/image/upload/v1588066071/fogg-coffee-break_iuhchq.png";
 
 export default function Hero() {
   return (
     <div className="flex flex-column w-full border-b-1">
       <img className="w-1/2 sm:w-full" src={imgUrl}></img>
-      <div className="w-1/2 sm:hidden flex items-center text-center">
+      <div className="w-1/2 sm:hidden flex items-center text-center bg-ls-ufo-green">
         <h1 className="lg:text-5xl md:text-3xl sm:text-2xl">A blog about living life sustainably as an individual.</h1>
       </div>
     </div>

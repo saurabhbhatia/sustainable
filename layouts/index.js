e76ed34ca0b6c92446d2536b5bccd87665dcd309
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-04-29 20:58:38 +1000

    add gtm

--- layouts/index.js
+++ layouts/index.js
@@ -1,19 +1,26 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Head from 'next/head'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-K9D9H4G'
+}
+
+TagManager.initialize(tagManagerArgs);
 
 const DefaultLayout = ({ children }) => (
-    <div>
-      <Head>
-        <title>Sustainable Life</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <header>
-        <Navbar />
-      </header>
-      <main>{children}</main>
-      <Footer />
-    </div>
+  <div>
+    <Head>
+      <title>Sustainable Life</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <Navbar />
+    </header>
+    <main>{children}</main>
+    <Footer />
+  </div>
 );
 
 export default DefaultLayout;

4fc02e7ff5179abb2d69914ae5bd5c7198738384
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-05-02 23:10:56 +1000

    add Google analytics

--- layouts/index.js
+++ layouts/index.js
@@ -1,19 +1,34 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Head from 'next/head'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import { initGA, logPageView } from "../utils/analytics";
+import { Component } from "react";
 
-const DefaultLayout = ({ children }) => (
-    <div>
-      <Head>
-        <title>Sustainable Life</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <header>
-        <Navbar />
-      </header>
-      <main>{children}</main>
-      <Footer />
-    </div>
-);
-
-export default DefaultLayout;
+export default class DefaultLayout extends Component {
+  componentDidMount () {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
+  
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Sustainable Life</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <header>
+          <Navbar />
+        </header>
+        <main>{this.props.children}</main>
+        <Footer />
+      </div>
+    );
+  }
+}

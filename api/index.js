71dd6277908d3fef54cac231d0fbf45cd3403b6c
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-05-16 00:10:14 +1000

    format files

--- api/index.js
+++ api/index.js
@@ -1,63 +1,63 @@
-import Prismic from 'prismic-javascript';
-import { PRISMIC_API_URL } from '../config';
+import Prismic from "prismic-javascript";
+import { PRISMIC_API_URL } from "../config";
 
-const getBlogPostsAPI = async params => {
-    try {
-      // We initialise the API with Prismic's kit
-      const API = await Prismic.api(PRISMIC_API_URL);
-      // Here we just query the documents with a filter of only returning
-      // the type of blog_post and order them. Full docs can be found here:
-      // https://github.com/prismicio/prismic-javascript#query-the-content
-      const response = await API.query(
-        Prismic.Predicates.at('document.type', 'blog_post'),
-        {
-          orderings: '[my.blog_post.date desc]',
-          ...params
-          // params will be extra parameters we can pass through with api calls
-          // such as how many documents to return
-        }
-      );
-      return response;
-    } catch (error) {
-      return error;
-    }
-  };
+const getBlogPostsAPI = async (params) => {
+  try {
+    // We initialise the API with Prismic's kit
+    const API = await Prismic.api(PRISMIC_API_URL);
+    // Here we just query the documents with a filter of only returning
+    // the type of blog_post and order them. Full docs can be found here:
+    // https://github.com/prismicio/prismic-javascript#query-the-content
+    const response = await API.query(
+      Prismic.Predicates.at("document.type", "blog_post"),
+      {
+        orderings: "[my.blog_post.date desc]",
+        ...params,
+        // params will be extra parameters we can pass through with api calls
+        // such as how many documents to return
+      }
+    );
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
 
-  const getBlogPostAPI = async slug => {
-    try {
-      const API = await Prismic.api(PRISMIC_API_URL);
-      // we pass up the slug to request the correct post
-      const response = await API.query(
-        Prismic.Predicates.at('my.blog_post.uid', slug)
-      );
-      return response.results[0];
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  };
+const getBlogPostAPI = async (slug) => {
+  try {
+    const API = await Prismic.api(PRISMIC_API_URL);
+    // we pass up the slug to request the correct post
+    const response = await API.query(
+      Prismic.Predicates.at("my.blog_post.uid", slug)
+    );
+    return response.results[0];
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
 
-  const getHomePage = async params => {
-    try {
-      const API = await Prismic.api(PRISMIC_API_URL);
-      const response = await API.getSingle('homepage');
-      return response;
-    } catch (error) {
-      return error;
-    }
-  };
-  
-  const getStaticPage = async slug => {
-    try {
-      const API = await Prismic.api(PRISMIC_API_URL);
-      const response = await API.query(
-        Prismic.Predicates.at("my.static_pages.uid", slug)
-      );
-      return response.results[0];
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  };
-  
-  export { getBlogPostsAPI, getBlogPostAPI, getHomePage, getStaticPage };
+const getHomePage = async (params) => {
+  try {
+    const API = await Prismic.api(PRISMIC_API_URL);
+    const response = await API.getSingle("homepage");
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getStaticPage = async (slug) => {
+  try {
+    const API = await Prismic.api(PRISMIC_API_URL);
+    const response = await API.query(
+      Prismic.Predicates.at("my.static_pages.uid", slug)
+    );
+    return response.results[0];
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export { getBlogPostsAPI, getBlogPostAPI, getHomePage, getStaticPage };
